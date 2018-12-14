6f1a430249f6b3552ad7e55bacdbb11a36dfb1fa
teuteuguy <timothee.cruse@gmail.com>
2018-12-14 12:51:10 +0800

    oops on appvars

--- source/console/src/assets/appVariables.js
+++ source/console/src/assets/appVariables.js
@@ -1,23 +1,12 @@
 'use strict';
 
-// const appVariables = {
-//     S3_DATA_BUCKET: '',
-//     IDENTITY_POOL_ID: '',
-//     USER_POOL_ID: '',
-//     USER_POOL_CLIENT_ID: '',
-//     IOT_COGNITO_POLICY: '',
-//     REGION: '',
-//     APP_SYNC_GRAPHQL_ENDPOINT: ''
-// };
-
 const appVariables = {
-    IOT_ENDPOINT: 'a3kex7ow4mkw1m-ats.iot.us-east-1.amazonaws.com',
-    S3_DATA_BUCKET: 'sputnik-cfstackfordatabucket-1f7meh8yb-databucket-1uldjlgb2h0rk',
-    VERSION: 'v0.9',
-    IDENTITY_POOL_ID: 'us-east-1:9723f43f-4012-41e2-9e7c-20ed73e30a1c',
-    USER_POOL_ID: 'us-east-1_BiZ8K6Hpc',
-    USER_POOL_CLIENT_ID: '5ef18upsc2n7qj9cke8bo8dnn2',
-    IOT_COGNITO_POLICY: 'sputnik-CFStackForCognito-1AHSUZTHQ7PL9-websiteCognitoIoTPolicy-HU862LV1P327',
-    REGION: 'us-east-1',
-    APP_SYNC_GRAPHQL_ENDPOINT: 'https://lchzck6wxredlavny3afwybt7a.appsync-api.us-east-1.amazonaws.com/graphql'
+    S3_DATA_BUCKET: '',
+    IDENTITY_POOL_ID: '',
+    USER_POOL_ID: '',
+    USER_POOL_CLIENT_ID: '',
+    IOT_COGNITO_POLICY: '',
+    REGION: '',
+    APP_SYNC_GRAPHQL_ENDPOINT: ''
 };
+
