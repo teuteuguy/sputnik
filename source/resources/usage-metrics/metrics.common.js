9cac734ed0e6358d158146d838f0d12c54bf33b1
teuteuguy <timothee.cruse@gmail.com>
2018-12-19 15:13:35 +0800

    Update

--- source/resources/usage-metrics/metrics.common.js
+++ source/resources/usage-metrics/metrics.common.js
@@ -1,21 +1,5 @@
-/*********************************************************************************************************************
- *  Copyright 2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.                                           *
- *                                                                                                                    *
- *  Licensed under the Amazon Software License (the "License"). You may not use this file except in compliance        *
- *  with the License. A copy of the License is located at                                                             *
- *                                                                                                                    *
- *      http://aws.amazon.com/asl/                                                                                    *
- *                                                                                                                    *
- *  or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES *
- *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions    *
- *  and limitations under the License.                                                                                *
- *********************************************************************************************************************/
-
-/**
- * @author Solution Builders
- */
-
 'use strict';
+
 const https = require('https');
 const AWS = require('aws-sdk');
 const moment = require('moment');
