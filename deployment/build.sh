2c00eb4252771c7fb050dc6bf7999ef710783048
teuteuguy <timothee.cruse@gmail.com>
2019-08-28 12:02:48 +0800

    Removed the zipped model from the Repo
    Now the build script downloads the model and zips it.

--- deployment/build.sh
+++ deployment/build.sh
@@ -40,3 +40,4 @@ $template_dir/scripts/02-cf-custom-resource-lambda.sh $source_dir $dist_dir
 $template_dir/scripts/03-website.sh $source_dir $dist_dir
 $template_dir/scripts/04-services-lambda.sh $source_dir $dist_dir
 $template_dir/scripts/05-greengrass-lambdas.sh $source_dir $dist_dir
+$template_dir/scripts/06-models.sh $dist_dir
